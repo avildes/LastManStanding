c002e6539a912a23273196cac46f43c33b36085d
Eduardo Bastos <eduardorochacg@gmail.com>
2014-04-05 16:04:11 -0300

    Ajustando .gitignore e incluindo arquivos que faltavam para o projeto funcionar

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -3,140 +3,23 @@ using System.Collections;
 
 public class FollowPlayer : MonoBehaviour {
 
-	public Transform target;//set target from inspector instead of looking in Update
 	public float speed = 5f;
 
-	//public Vector3 speed = new Vector3(3, 3, 0);
-	public Vector3 direction = new Vector3();
-	private Vector2 movement;
-
-	private Vector3 actualDirection = new Vector3(0,0,0);
-
-	void Start () {
-		
-	}
-
-	bool check = true;
-	public float waitAndCheck = .5f;
-
-	IEnumerator CheckNasParadas(){
-
-		transform.LookAt(target.position);
-		//transform.LookAt (Vector3 (0, target.position.y, target.position.z));
-
-		//Q/uaternion neededRotation = Quaternion.LookRotation(target.position - transform.position);
-		
-		//Quaternion interpolatedRotation = Quaternion.Slerp(new Quaternion(transform.position.x, transform.position.y, transform.position.z,0), neededRotation, (Time.deltaTime * .2f));
-
-		
-		direction = target.position - transform.position;
-
-		movement = speed * transform.forward;
-
-		//transform.Rotate(new Vector3(0,-90,0),Space.Self);//correcting the original rotation
-
-		transform.Rotate(new Vector3(0,-90,0),Space.Self);//correcting the original rotation
-
-
-
-		yield return new WaitForSeconds(waitAndCheck);
-		check = true;
-	}
-
-	void Update(){
-
+	private GameObject target;
 	
-		//rotate to look at the player
+	private Vector3 _direction;
 
-		/*
-		 * 
-		 * transform.LookAt(target.position);
-		transform.Rotate(new Vector3(0,-90,0),Space.Self);//correcting the original rotation
-		
-		direction = target.position - transform.position;
-		/*
-		if(direction.Equals(actualDirection)){
-			*
-			**
-			**
-			**
-			**
-			*movement = speed * direction;/*
-		}
-		else
-		{
-			if(rigidbody2D.velocity.Equals(new Vector2(0,0))){
-				actualDirection = direction;
-			}
-
-
-		}*/
-
-
-		if (check)
-		{
-			check = false;
-			StartCoroutine(CheckNasParadas());
-		}
-
-
-
-		/*
-		//move towards the player
-		//if (Vector3.Distance(transform.position,target.position)>1f){//move if distance from target is greater than 1
-			movement = new Vector2(
-				(speed.x*Time.deltaTime) - target.position.x,
-				(speed.y*Time.deltaTime) - target.position.y);
-		//}
-
-		*/
-	}
-
-	void FixedUpdate()
+	void Start ()
 	{
-
-		//MovePosition ();
-		MoveVelocity ();
-		//MoveForce ();
-		//MoveTranslate (); <-- n presta...
-
-
+		target = GameObject.FindGameObjectWithTag ("Player");
 	}
 
-	void MoveForce(){
-		rigidbody2D.AddForce( movement);
+	void Update()
+	{
+		if(target != null)
+		{
+			_direction = target.transform.position - transform.position;
+			rigidbody2D.velocity = _direction.normalized * speed;
+		}
 	}
-
-	void MoveVelocity(){
-
-		// Apply movement to the rigidbody
-		rigidbody2D.velocity = movement;
-		//rigidbody2D.velocity = new Vector2((transform.forward.x * speed.x),(transform.forward.y * speed.y));
-		//rigidbody2D.AddForce (new Vector2((speed.x * transform.forward.x), (speed.y * transform.forward.y)));//speed * (new Vector2(target.position.x, target.position.y)).normalized);
-		
-		
-		//Debug.Log (target.position.x);
-		
-		//Debug.Log (rigidbody2D.velocity.x);
-		//Debug.Log (rigidbody2D.velocity.y);
-	}
-
-	void MoveTranslate (){
-		Debug.Log ("Translate");
-		speed = 10f;
-		transform.Translate(new Vector3(speed* Time.deltaTime,0,0) );
-	}
-
-	void MovePosition (){
-		Debug.Log ("Position");
-		speed = 5f;
-
-		Vector3 dir = target.position - transform.position;
-		
-		//dir.Normalize();
-		
-		// Move ourselves in that direction
-		transform.position += (dir * (speed * Time.deltaTime));
-	}
-
 }
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+
+	public GameObject mob;
+
+	// Use this for initialization
+	void Start ()
+	{
+		StartCoroutine(SpawnMob());
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	}
+
+	IEnumerator SpawnMob()
+	{
+		Instantiate(mob, transform.position, Quaternion.identity);
+
+		yield return new WaitForSeconds(2);
+
+		StartCoroutine(SpawnMob());
+	}
+}
