1ac6f4700c72016802ad3ea47acde0dad4fd3abd
Antonio Vildes Barbosa <avildes@gmail.com>
2014-04-05 23:23:03 -0300

    Created some new scenes
    Deleted some unused scenes, created the opening and menu scenes (but I
    still need to finish the screen flux)

---
+++ Assets/Scripts/OpeningScreenScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpeningScreenScript : MonoBehaviour
+{
+
+	public int levelNumberInBuild;
+
+	void OnTouchDown()
+	{
+		Application.LoadLevel(levelNumberInBuild);
+	}
+}
