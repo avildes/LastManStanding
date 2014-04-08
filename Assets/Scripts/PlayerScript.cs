5dd29dc68423f5eafd39eb0a45307a0d0385a904
Eduardo Bastos <eduardorochacg@gmail.com>
2014-04-07 22:32:36 -0300

    Correção: Não é mais possível mudar a direção do movimento durante o pulo

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -7,58 +7,69 @@ using System.Collections;
 public class PlayerScript : MonoBehaviour
 {
 	/// <summary>
-	/// 1 - The speed of the ship
+	/// Velocidade do player
 	/// </summary>
 	public Vector2 speed = new Vector2(50, 50);
 	
+    /// <summary>
+    /// Texto usado para mostrar o tempo final
+    /// </summary>
     public GUIText endTimeText;
 
-	// 2 - Store the movement
+	/// <summary>
+    /// Vetor que representa o movimento do player
+    /// </summary>
 	private Vector2 movement;
 
-	private float totalTime;
+    /// <summary>
+    /// Vetor de direçao do movimento do jogador no momento do pulo
+    /// </summary>
+    private Vector2 jumpMomentVector;
 
+    /// <summary>
+    /// Flag que indica se o player esta pulando
+    /// </summary>
     private bool isJumping;
+
+    /// <summary>
+    /// Tempo total de jogo
+    /// </summary>
+    private float totalTime;
 	
 	void Update()
 	{
-		// 3 - Retrieve axis information
+		// Recupera o input do jogador
 		float inputX = Input.GetAxis("Horizontal");
         float inputY = Input.GetAxis("Vertical");
         
-        // 4 - Movement per direction
+        // Seta o vetor de movimento de acordo com o input
         movement = new Vector2(
             speed.x * inputX,
             speed.y * inputY);
 
+        // Se o player esta pulando, ele pode mover-se apenas na mesma direçao com que iniciou o pulo
+        if (isJumping && jumpMomentVector.magnitude != 0 && !_IsInSameDirection(movement, jumpMomentVector))
+        {
+            movement = new Vector2(0, 0);
+        }
+
         if (Input.GetKeyDown(KeyCode.Space) && !isJumping)
         {
+            jumpMomentVector = new Vector2(inputX, inputY);
             StartCoroutine(JumpCoroutine ());
         }
 	}
 	
 	void FixedUpdate()
 	{
-		// 5 - Move the game object
 		rigidbody2D.velocity = movement;
 	}
-
-    IEnumerator JumpCoroutine()
-    {
-        isJumping = true;
-        renderer.material.color = new Color(0, 255, 255);
-
-        yield return new WaitForSeconds(1);
-
-        isJumping = false;
-        renderer.material.color = new Color(255, 0, 0);
-    }
 
 	void OnCollisionEnter2D(Collision2D hit)
 	{
 		if (hit.gameObject.tag == "Enemy")
 		{
-            Destroy();
+            OnPlayerDeath();
 		}
 	}
 
@@ -68,15 +79,45 @@ public class PlayerScript : MonoBehaviour
         {
             if(!isJumping)
             {
-                Destroy();
+                OnPlayerDeath();
             }
         }
     }
 
-    void Destroy()
+    /// <summary>
+    /// Metodo que realiza as atividades relacionadas a morte do player
+    /// </summary>
+    private void OnPlayerDeath()
     {
         totalTime = TimerScript.Instance.GetTotalTime();
         endTimeText.text = string.Format("TIME {0:f2}", totalTime);
         Destroy(this);
+    }
+
+    /// <summary>
+    /// Verifica se dois vetores estao na mesma direçao
+    /// </summary>
+    private bool _IsInSameDirection(Vector2 vector1, Vector2 vector2)
+    {
+        Vector2 vector1Normalized = vector1.normalized;
+        Vector2 vector2Normalized = vector2.normalized;
+
+        // Os vetores estao no mesmo sentido ou em sentidos opostos
+        return (vector1Normalized.x == vector2Normalized.x && vector1Normalized.y == vector2Normalized.y) ||
+            (vector1Normalized.x == -vector2Normalized.x && vector1Normalized.y == -vector2Normalized.y);
+    }
+
+    /// <summary>
+    /// Corotina de pulo
+    /// </summary>
+    private IEnumerator JumpCoroutine()
+    {
+        isJumping = true;
+        renderer.material.color = new Color(0, 255, 255);
+        
+        yield return new WaitForSeconds(0.5f);
+        
+        isJumping = false;
+        renderer.material.color = new Color(255, 0, 0);
     }
 }
