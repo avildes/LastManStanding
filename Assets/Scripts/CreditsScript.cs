d5ac0ee03d59b9f9906940a064f9d92d33b47ab0
Eduardo Bastos <eduardorochacg@gmail.com>
2014-07-16 19:15:42 -0300

    Correções gerais

---
+++ Assets/Scripts/CreditsScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreditsScript : MonoBehaviour
+{
+    public AudioClip buttonSound;
+
+    private AudioSource audioSource;
+
+    void Start()
+    {
+		audioSource = gameObject.GetComponent<AudioSource>();
+    }
+
+    void Update ()
+    {
+        if (Input.anyKey)
+        {
+			audioSource.PlayOneShot(buttonSound);
+            StartCoroutine(LoadMenu());
+        }
+	}
+
+    IEnumerator LoadMenu()
+    {
+        gameObject.GetComponent<Animator>().SetTrigger("CreditsExit");
+
+        yield return new WaitForSeconds(.75f);
+
+        Application.LoadLevel("Menu");
+    }
+}
